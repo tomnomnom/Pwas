239680ff6e7d6dbb255154499e02118226f09144
Tom Hudson <mail@tomnomnom.com>
2011-09-04 11:59:47 +0000

    Removed built in static file serving; Discrete callback args

--- Examples/Hello.php
+++ Examples/Hello.php
@@ -3,8 +3,7 @@ require __DIR__.'/../Pwas/Init.php';
 
 $app = new \Pwas\Http\Server('127.0.0.1', 9090);
 
-$app->accept(function($server){
-  $response = $server->getResponse();
+$app->accept(function($request, $response){
   $response->sendHeaders(200, array(
     'Content-Type' => 'text/html'
   ));
--- Pwas/Http/Server.php
+++ Pwas/Http/Server.php
@@ -1,47 +1,17 @@
 <?php
 namespace Pwas\Http;
 class Server extends \Pwas\Tcp\Server {
-  protected $request;
-  protected $response;
-
-  protected $static_files_path = null;
 
   public function callUserFunction($fn){
-    $this->request = new Request();
-    $this->request->parseHeaders($this->read());
+    $request = new Request();
+    $request->parseHeaders($this->read());
 
-    $this->response = new Response($this);
+    $response = new Response($this);
 
-    if (!$this->serveStaticFiles()){
-      parent::callUserFunction($fn);
-    }
+    call_user_func_array($fn, array(
+      $request, $response
+    ));
   }
-
-  public function getResponse(){
-    return $this->response;
-  }
-  public function getRequest(){
-    return $this->request;
-  }
-
-  public function serveStaticFilesFrom($path){
-    $this->static_files_path = $path; 
-  }
-
-  protected function serveStaticFiles(){
-    if (!$this->static_files_path) return false;
-    $path = $this->static_files_path . '/' . $this->serverVar('path');
-    if (!file_exists($path)) return false;
-    if($this->write(file_get_contents($path))) return true;
-    return false;
-  }
-
-  public function serveStaticFile($file){
-    $file = "{$this->static_files_path}/{$file}";
-    if($this->write(file_get_contents($file))) return true;
-    return false;
-  }
-
 }
 
 
